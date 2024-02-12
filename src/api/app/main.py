c5c14ad1a1eb65e7d55b953309c41e2cafe43376
Jeferson Oliveira <59900069+jefersonrjrn@users.noreply.github.com>
2024-02-12 14:44:41 -0300

    v1.1 (#10)
    * add terraform initial configuration
    
    * add run basic instance
    
    * add cloud run service account
    
    * Delete src/terraform/.terraform directory
    
    * Add docker repo Artifact Registry
    
    * Add cloud build basic configuration
    
    * Add bigquery dataset and table
    
    * Add pubsub topic and subscription
    
    * Update BQ schema
    
    * api initial setup
    
    * Add cloudbuild config file
    
    * Fix dockerfile location on build step
    
    * Fix container port
    
    * Add new route to api
    
    * Fix function arguments
    
    * Add simple integration test
    
    * Add integration step on cloudbuild

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -6,14 +6,15 @@ app = FastAPI()
 
 
 @app.get("/get-flights/{month}")
-def get_flights():
+
+def get_flights_by_month(month: str):
     # Construct a BigQuery client object.
     client = bigquery.Client()
 
     query = f"""
         SELECT flight_id,flight_origin,flight_destination,flight_month
         FROM `angelic-digit-302818.ds_challenge.flights`
-        WHERE flight_month='August'
+        WHERE flight_month='{month}'
         LIMIT 20
     """
     rows = client.query_and_wait(query)  # Make an API request.
@@ -29,4 +30,32 @@ def get_flights():
             "flight_month": row["flight_month"]
         })
     print(flight_list)
+    return flight_list
+
+@app.get("/get-flights")
+
+def get_flights():
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    query = f"""
+        SELECT flight_id,flight_origin,flight_destination,flight_month
+        FROM `angelic-digit-302818.ds_challenge.flights`
+
+        LIMIT 20
+    """
+    rows = client.query_and_wait(query)  # Make an API request.
+    flight_list = []
+
+    print("The query data:")
+    for row in rows:
+        # Row values can be accessed by field name or index.
+        flight_list.append({
+            "flight_id": row["flight_id"],
+            "flight_origin": row["flight_origin"],
+            "flight_destination": row["flight_destination"],
+            "flight_month": row["flight_month"]
+        })
+    print(flight_list)
+
     return flight_list
